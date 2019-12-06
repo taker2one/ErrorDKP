c407637e42241ff758f46a7aec3f70d4563246a0
Manuel Ebner <manuel.ebner@liwest.at>
2019-12-06 20:10:15 +0100

    TExture

--- modules/lootsurvey.lua
+++ modules/lootsurvey.lua
@@ -68,10 +68,12 @@ function LootSurvey:Update(data)
     local f = LootSurvey:GetFrame()
     for i,v in ipairs(DemoSurveyData.items) do
         entryFrame = LootSurvey:GetEntry(i)
-        entryFrame:SetPoint("TOPLEFT", f, "TOPLEFT", 10, 80+(-80*i))
+        entryFrame:SetPoint("TOPLEFT", f, "TOPLEFT", 11, 65+(-90*i))
+
+        entryFrame.ItemText:SetText(v.itemLink)
     end
 
-    f:SetHeight(#DemoSurveyData.items*80+50)
+    f:SetHeight(#DemoSurveyData.items*90+30)
 end
 
 -- 'function LootSurvey:DoCellUpdateIcon(rowFrame, cellFrame, data, cols, row, realrow, column, fShow, self, ...)
@@ -87,23 +89,6 @@ function LootSurvey:DoCellUpdateItemAndButtons(rowFrame, cellFrame, data, cols, 
 end
 
 
-function LootSurvey:GetButtonSet(index)
-    local f = LootSurvey:GetFrame()
-    if not f.ButtonSets[index] then
-        local setFrame = CreateFrame("Frame", "LootSurveyBtnSetFrame"..index, f)
-        --Create new ButtonSet on index
-        setFrame.MainBtn = core:CreateButton(setFrame, "MainSpecBtn", "Main")
-        setFrame.MainBtn:SetPoint("TOPLEFT", setFrame, "TOPLEFT", 85, 40)
-        setFrame.SecBtn = core:CreateButton(setFrame, "SecSpecBtn", "Second")
-        setFrame.SecBtn:SetPoint("LEFT", setFrame.MainBtn, "RIGHT")
-        setFrame.PassBtn = core:CreateButton(setFrame, "PassBtn", "Pass")
-        setFrame.PassBtn:SetPoint("LEFT", setFrame.SecBtn, "RIGHT")
-
-        table.insert(f.ButtonSets, index, setFrame)
-    end
-    return f.ButtonSets[index]
-end
-
 function LootSurvey:CreateFrame()
     local f = core:CreateDefaultFrame("ErrorDKPLootSurveyFrame", "Loot Survey", 420, 375)
     core.UI.LootSurvey = f
@@ -118,6 +103,10 @@ function LootSurvey:CreateFrame()
     return f
 end
 
+function LootSurvey:OnClickEntryButton(button, index)
+    core:PrintDebug("LootSurvey:OnClickEntryButton", button, index)
+end
+
 function LootSurvey:GetFrame()
     local f = core.UI.LootSurvey or self:CreateFrame()
     return f
@@ -127,26 +116,46 @@ function LootSurvey:GetEntry(index)
     local f = LootSurvey:GetFrame()
     if not f.Entries[index] then
         f.Entries[index] = LootSurvey:CreateEntry("LootSurveyEntry"..index)
+        f.Entries[index].Index = index
     end
     return f.Entries[index]
 end
 
 function LootSurvey:CreateEntry(name)
     local f = CreateFrame("Frame", name, UIParent)
-    f:SetSize(500,80)
+    f:SetSize(478,80)
+
+    f.bg = f:CreateTexture(nil, "BACKGROUND")
+    f.bg:SetAllPoints(f)
+    f.bg:SetTexture(1,0.5,0.5,0.5)
    
     local icon = CreateFrame("Frame", nil, f)
     icon:SetPoint("LEFT", f, "LEFT")
     icon:SetSize(80,80)
-    icon:SetTexture("Interface\\InventoryItems\\WoWUnknownItem01")
+    icon.texture = icon:CreateTexture(nil, "ARTWORK")
+    icon.texture:SetAllPoints(icon)
+    icon.texture:SetTexture("Interface\\InventoryItems\\WoWUnknownItem01")
 
-    --Create new ButtonSet on index
+    f.ItemText = f:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
+    f.ItemText:SetText("Text")
+    f.ItemText:SetPoint("TOPLEFT", f, "TOPLEFT", 85, -15)
+
     f.MainBtn = core:CreateButton(f, "MainSpecBtn", "Main")
     f.MainBtn:SetPoint("TOPLEFT", f, "TOPLEFT", 85, -40)
     f.SecBtn = core:CreateButton(f, "SecSpecBtn", "Second")
     f.SecBtn:SetPoint("LEFT", f.MainBtn, "RIGHT")
     f.PassBtn = core:CreateButton(f, "PassBtn", "Pass")
     f.PassBtn:SetPoint("LEFT", f.SecBtn, "RIGHT")
+
+    f.MainBtn:SetScript("OnClick", function()
+        LootSurvey:OnClickEntryButton("MAIN", f.Index)
+    end)
+    f.SecBtn:SetScript("OnClick", function()
+        LootSurvey:OnClickEntryButton("SECOND", f.Index)
+    end)
+    f.PassBtn:SetScript("OnClick", function()
+        LootSurvey:OnClickEntryButton("PASS", f.Index)
+    end)
 
     return f
 end
