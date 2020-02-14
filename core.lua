20e315c6b14fbfd7be49ebbf674d5559372ecbc4
Ebner Manuel <manuel.ebner@liwest.at>
2020-02-14 13:29:18 +0100

    Change Shaman color to retail blue

--- core.lua
+++ core.lua
@@ -70,7 +70,7 @@ core.ClassColors = {
 	["MAGE"] = { r = 0.25, g = 0.78, b = 0.92, hex = "40C7EB" },
 	["PRIEST"] = { r = 1, g = 1, b = 1, hex = "FFFFFF" },
 	["ROGUE"] = { r = 1, g = 0.96, b = 0.41, hex = "FFF569" },
-	["SHAMAN"] = { r = 0.96, g = 0.55, b = 0.73, hex = "F58CBA" },
+	["SHAMAN"] = { r = 0.00, g = 0.44, b = 0.87, hex = "0070DE" },
 	["PALADIN"] = { r = 0.96, g = 0.55, b = 0.73, hex = "F58CBA" },
 	["WARLOCK"] = { r = 0.53, g = 0.53, b = 0.93, hex = "8787ED" },
 	["WARRIOR"] = { r = 0.78, g = 0.61, b = 0.43, hex = "C79C6E" }
